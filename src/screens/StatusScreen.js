e99bb3bd90202beb9d955408ece6bc23d3b5bcdb
山下拓 <yamashitataku@taku.local>
2020-10-17 17:20:08 +0900

    select data from database

--- src/screens/StatusScreen.js
+++ src/screens/StatusScreen.js
@@ -1,22 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { Asset } from 'expo-asset';
 import * as SQLite from 'expo-sqlite';
 
-const assetHtml = require('./index.html');
+
 const db = SQLite.openDatabase('db');
 
 
-const StatusScreen = ({ navigation }) => {
-  const [text, setText] = useState('');
+const Items = () => {
+  const [items, setItems] = useState(null);
 
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'select * from users where id = 1;',
+        null,
+        (_, { rows: { _array } }) => setItems(_array),
+      );
+    });
+  }, []);
+
+  if (items === null || items.length === 0) {
+    return null;
+  }
+
+  return (
+    <View style={styles.todolist}>
+      <View style={styles.todo}>
+        <Text>心：</Text>
+        {items.map(({ id, heart }) => (
+          <Text key={id}>{heart}</Text>
+        ))}
+      </View>
+      <View style={styles.todo}>
+        <Text>技：</Text>
+        {items.map(({ id, technique }) => (
+          <Text key={id}>{technique}</Text>
+        ))}
+      </View>
+      <View style={styles.todo}>
+        <Text>体：</Text>
+        {items.map(({ id, body }) => (
+          <Text key={id}>{body}</Text>
+        ))}
+      </View>
+      <View style={styles.todoQuit}>
+        <Text style={styles.quit}>捨：</Text>
+        {items.map(({ id, quit }) => (
+          <Text style={styles.quit} key={id}>{quit}</Text>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const StatusScreen = () => {
   return (
     <View style={styles.container}>
       <WebView
-        source={require('./index.html')}
+        source={require('./graph.html')}
         startInLoadingState
       />
+      <Items />
     </View>
   );
 };
@@ -25,38 +70,24 @@ const StatusScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fffbf6',
   },
-  title: {
-    color: '#3f3f36',
-    textAlign: 'center',
+  todolist: {
     marginTop: 50,
+    marginBottom: 80,
   },
-  text: {
-    lineHeight: 40,
-  },
-  contents: {
-    marginTop: 80,
-    marginBottom: 50,
-    alignSelf: 'center',
-  },
-  content: {
+  todo: {
     flexDirection: 'row',
-    margin: 35,
-  },
-  input: {
-    width: 250,
-    padding: 10,
-    borderBottomColor: '#3f3f36',
-    borderBottomWidth: 1,
-  },
-  button: {
-    width: 100,
-    height: 40,
-    textAlign: 'center',
+    margin: 20,
     alignSelf: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#EBEBEB',
+  },
+  todoQuit: {
+    flexDirection: 'row',
+    marginTop: 50,
+    alignSelf: 'center',
+  },
+  quit: {
+    color: '#FF6384',
   },
 });
 
